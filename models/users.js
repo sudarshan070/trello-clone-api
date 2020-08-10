a27a7ec83030fa18c8c20e948cbd3d6ea7a4a2a8
Sudarshan shinde <shindesudarshan070@gmail.com>
2020-08-10 23:24:37 +0530

    login user

--- models/users.js
+++ models/users.js
@@ -32,9 +32,9 @@ var userSchema = new Schema({
 userSchema.pre("save", async function (next) {
     try {
         if (this.password && this.isModified('password')) {
-            this.password = await bcrypt.hash(this.password, 10)
+            this.password = await bcrypt.hashSync(this.password, 10)
         }
-        next()
+        return next()
     } catch (error) {
         next(error)
     }
--- routes/v1/users.js
+++ routes/v1/users.js
@@ -21,4 +21,39 @@ router.post("/", async (req, res, next) => {
   }
 })
 
+
+router.post("/login", async (req, res, next) => {
+  var { password, email } = req.body.user
+  if (!email || !password) {
+    return res.status(400).json({
+      success: false,
+      error: "Email and Password are required"
+    })
+  }
+  try {
+    var user = await User.findOne({ email })
+    console.log(user, "user is here")
+    if (!user) {
+      return res.status(400).json({
+        success: false,
+        error: "Email is wrong"
+      })
+    }
+    if (!user.verifyPassword(password) && !user.verifyPassword(password) == "") {
+      return res.status(400).json({
+        success: false,
+        error: "Password is wrong"
+      })
+    }
+    var token = await auth.generateJWT(user)
+    res.status(201).json({
+      email: user.email,
+      name: user.name,
+      token
+    })
+  } catch (error) {
+    next(error)
+  }
+})
+
 module.exports = router;
