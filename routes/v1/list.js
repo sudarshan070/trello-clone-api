e9a6e5e4ba3506134158ae105a2e93cbe77a381a
Sudarshan shinde <shindesudarshan070@gmail.com>
2020-09-29 12:51:20 +0530

    update list

--- routes/v1/list.js
+++ routes/v1/list.js
@@ -10,25 +10,41 @@ var auth = require('../../middleware/auth')
 
 router.post("/:boardSlug/create", auth.verifyToken, async (req, res, next) => {
     try {
-
-        console.log('in list route');
         var boardSlug = req.params.boardSlug
-            req.body.list.memberId = req.user.userId
+        req.body.list.memberId = req.user.userId
         let board = await Board.findOne({ slug: boardSlug })
-            let boardId = board.id
-            req.body.list.boardId = boardId
+        let boardId = board.id
+        req.body.list.boardId = boardId
         var list = await List.create(req.body.list)
         console.log(list, "list is here")
-            if (list) {
-                let board = await Board.findByIdAndUpdate(
-                    boardId,
-                    {
-                        $addToSet: { lists: list.id },
-                    },
-                    { new: true }
-                );
-            }
-            return res.status(200).json({ list })
+        if (list) {
+            let board = await Board.findByIdAndUpdate(
+                boardId,
+                {
+                    $addToSet: { lists: list.id },
+                },
+                { new: true }
+            );
+        }
+        return res.status(200).json({ list })
+    } catch (error) {
+        next(error)
+    }
+})
+
+
+router.put('/:boardSlug/update/:slug', auth.verifyToken, async (req, res, next) => {
+    try {
+        var board = await Board.findOne({ slug: req.params.slug })
+        var list = await List.findOne({ slug: req.params.slug })
+        console.log(list, "list is here");
+
+        list = await List.findByIdAndUpdate(
+            list.id,
+            req.body.list,
+            { new: true }
+        )
+        res.json({ list })
     } catch (error) {
         next(error)
     }
